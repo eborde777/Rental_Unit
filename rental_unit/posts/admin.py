d35ab7334a6869037a63fb049735fb46ffcedbc7
Borde <Bordee@labcorp.com>
2018-04-20 16:14:11 -0400

    Change admin view

--- rental_unit/posts/admin.py
+++ rental_unit/posts/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from .models import State, City, Post, Type
 
 # Register your models here.
-admin.site.register(State)
+# admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Post)
 admin.site.register(Type)
+
+class StateAdmin(admin.ModelAdmin):
+    list_display = ['state_abbrv', 'state'] 
+    list_filter = ['state_abbrv', 'state'] 
+    search_fields = ['state']
+
+admin.site.register(State, StateAdmin)
